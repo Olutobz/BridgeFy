028db3f57d367f04d2c3b4043c3f54de17282a0e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-05-28 10:20:41 +0100

    setup webView

--- app/src/main/java/com/dev/olutoba/bridgefy/MainActivity.kt
+++ app/src/main/java/com/dev/olutoba/bridgefy/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.dev.olutoba.bridgefy
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.PermissionRequest
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.dev.olutoba.bridgefy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        webView = binding.webView
+        progressBar = binding.loadingIndicator
+        setupWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        with(webView) {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.allowFileAccess = true
+            settings.allowContentAccess = true
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.mediaPlaybackRequiresUserGesture = true
+            settings.setSupportMultipleWindows(true)
+            loadUrl(WEB_URL)
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.loadingIndicator.isVisible = false
+                super.onPageFinished(view, url)
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                super.onPermissionRequest(request)
+            }
+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private companion object {
+        // Replace with your website url
+        const val WEB_URL = "http://www.youtube.com"
     }
 }
